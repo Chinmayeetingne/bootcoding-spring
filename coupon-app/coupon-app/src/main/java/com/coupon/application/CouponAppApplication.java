ecf80d536426b57bce8e02c5bd12804d31fa63ac
Chinmayeetingne <rashitingne12@gmail.com>
2023-08-10 22:29:36 +0530

    Spring exercise

--- coupon-app/coupon-app/src/main/java/com/coupon/application/CouponAppApplication.java
+++ coupon-app/coupon-app/src/main/java/com/coupon/application/CouponAppApplication.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class CouponAppApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CouponAppApplication.class, args);
 	}
 	@Autowired
